9c30e9a42e9ca917cdb7472f1a94de0302fad4c5
Julia <git@juliapixel.com>
2025-09-16 15:54:29 -0300

    Make compile-time macros configurable
    Makes it so that dotenvy_macro::dotenv follows the same convention as dotenvy::load
    
    Also adds dotenvy_macro::option_dotenv

--- dotenv_codegen/src/lib.rs
+++ dotenv_codegen/src/lib.rs
@@ -1,68 +1,236 @@
-use dotenvy::EnvLoader;
+use dotenvy::{EnvLoader, EnvSequence};
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use std::env::{self, VarError};
-use syn::{parse::Parser, punctuated::Punctuated, spanned::Spanned, LitStr, Token};
+use std::io;
+use syn::{parse::Parse, Ident, LitBool, LitStr, Token};
 
+struct DotenvInput {
+    path: Option<LitStr>,
+    override_: bool,
+    var_name: LitStr,
+}
+
+impl Parse for DotenvInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let mut path = None;
+        let mut var_name = None;
+        let mut override_ = None;
+
+        while !input.is_empty() {
+            if let Ok(ident) = input.parse::<Ident>() {
+                input.parse::<Token![=]>()?;
+                match ident.to_string().as_str() {
+                    "path" => {
+                        if path.is_some() {
+                            return Err(syn::Error::new(
+                                ident.span(),
+                                "each attribute must be set only once",
+                            ));
+                        }
+                        path = Some(input.parse::<LitStr>()?);
+                    }
+                    "override_" => {
+                        if override_.is_some() {
+                            return Err(syn::Error::new(
+                                ident.span(),
+                                "each attribute must be set only once",
+                            ));
+                        }
+                        override_ = Some(input.parse::<LitBool>()?.value);
+                    }
+                    "var" => {
+                        if var_name.is_some() {
+                            return Err(syn::Error::new(
+                                ident.span(),
+                                "variable name must be set only once",
+                            ));
+                        }
+                        var_name = Some(input.parse::<LitStr>()?);
+                    }
+                    _ => {
+                        return Err(syn::Error::new(
+                            ident.span(),
+                            format!("unkown attribute: {ident}"),
+                        ))
+                    }
+                }
+            } else if let Ok(s) = input.parse::<LitStr>() {
+                if var_name.is_some() {
+                    return Err(syn::Error::new(
+                        s.span(),
+                        "unexpected token in macro input (variable name must be set only once)",
+                    ));
+                }
+                var_name = Some(s);
+            } else {
+                return Err(syn::Error::new(
+                    input.span(),
+                    "unexpected token in macro input",
+                ));
+            }
+            if !input.is_empty() {
+                input.parse::<Token![,]>()?;
+            }
+        }
+
+        let Some(var_name) = var_name else {
+            return Err(syn::Error::new(
+                input.span(),
+                "environment variable name not defined",
+            ));
+        };
+
+        Ok(Self {
+            path,
+            override_: override_.unwrap_or(true),
+            var_name,
+        })
+    }
+}
+
+/// Loads an environment variable from a file at compile time.
+///
+/// This macro will expand to the value of the named environment variable at compile
+/// time, yielding an expression of type `&'static str`. Use
+/// [`dotenvy::var`] instead if you want to read the value at runtime.
+///
+/// If the environment variable is not defined, then a compilation error will be
+/// emitted. To not emit a compile error, use the [`option_env!`](option_env)
+/// macro instead. A compilation error will also be emitted if the environment
+/// variable is not a valid Unicode string or if reading the .env file failed.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rust no_run
+///     assert_eq!(dotenv!("VARIABLE_1"), "value");
+/// ```
+///
+/// ```rust compile_fail
+///     const UNSER_VAR: &str = dotenv!("UNSET_VAR");
+/// ```
+///
+/// Custom attributes:
+///
+/// ```rust no_run
+///     // Does not override current env with .env contents
+///     const NOT_OVERRIDEN: &str = dotenv!("VARIABLE_1", override_ = false);
+///     // Reads from custom file path
+///     const CUSTOM_PATH: &str = dotenv!("VARIABLE_PROD", path = ".env.prod");
+///     // Specifying the `var` attribute
+///     const CUSTOM_PATH: &str = dotenv!(var = "VARIABLE_1");
+/// ```
 #[proc_macro]
-/// TODO: add safety warning
 pub fn dotenv(input: TokenStream) -> TokenStream {
     let input = input.into();
-    unsafe { dotenv_inner(input) }.into()
+    dotenv_inner(input).into()
 }
 
-unsafe fn dotenv_inner(input: TokenStream2) -> TokenStream2 {
-    let loader = EnvLoader::new();
-    if let Err(e) = unsafe { loader.load_and_modify() } {
-        let msg = e.to_string();
-        return quote! {
-            compile_error!(#msg);
-        };
-    }
+fn dotenv_inner(input: TokenStream2) -> TokenStream2 {
+    let args = match syn::parse2::<DotenvInput>(input) {
+        Ok(a) => a,
+        Err(e) => return e.into_compile_error(),
+    };
 
-    match expand_env(input) {
-        Ok(stream) => stream,
-        Err(e) => e.to_compile_error(),
+    let sequence = if args.override_ {
+        EnvSequence::EnvThenInput
+    } else {
+        EnvSequence::InputThenEnv
+    };
+
+    let path = args.path.map_or_else(|| "./.env".to_owned(), |p| p.value());
+    let var_name = args.var_name.value();
+
+    let loader = EnvLoader::new().path(&path).sequence(sequence).load();
+
+    match loader.as_ref().map(|l| l.get(&var_name)) {
+        Ok(Some(v)) => quote!(#v),
+        Ok(None) => {
+            let msg = format!("environment variable `{var_name}` not set");
+            quote! {
+                compile_error!(#msg)
+            }
+        }
+        Err(e) => {
+            let msg = e.to_string();
+            quote! {
+                compile_error!(#msg)
+            }
+        }
     }
 }
 
-fn expand_env(input_raw: TokenStream2) -> syn::Result<TokenStream2> {
-    let args = <Punctuated<syn::LitStr, Token![,]>>::parse_terminated
-        .parse(input_raw.into())
-        .expect("expected macro to be called with a comma-separated list of string literals");
+/// Optionally loads an environment variable from a file at compile time.
+///
+/// If the named environment variable is present at compile time, this will expand
+/// into an expression of type `Option<&'static str>` whose value is `Some` of the
+/// value of the environment variable (a compilation error will be emitted if the
+/// environment variable is not a valid Unicode string or if reading the .env file
+/// failed). If either the environment variable or the .env file is not present,
+/// then this will expand to `None`. Use [`dotenvy::var`] instead if
+/// you want to read the value at runtime.
+///
+/// A compile time error is only emitted when using this macro if the environment
+/// variable exists and is not a valid Unicode string or if reading the .env file
+/// failed. To also emit a compile error if the environment variable is not present,
+/// use the [`dotenv!`](dotenv) macro instead.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rust no_run
+///     assert_eq!(option_dotenv!("UNSET_VAR"), None);
+///     assert_eq!(option_dotenv!("SET_VAR"), Some("value"));
+/// ```
+///
+/// Custom attributes:
+///
+/// ```rust no_run
+///     // Does not override current env with .env contents
+///     const NOT_OVERRIDEN: Option<&str> = option_dotenv!("VARIABLE_1", override_ = false);
+///     // Reads from custom file path
+///     const CUSTOM_PATH: Option<&str> = option_dotenv!("VARIABLE_PROD", path = ".env.prod");
+///     // Specifying the `var` attribute
+///     const CUSTOM_PATH: Option<&str> = option_dotenv!(var = "VARIABLE_1");
+/// ```
+#[proc_macro]
+pub fn option_dotenv(input: TokenStream) -> TokenStream {
+    let input = input.into();
+    option_dotenv_inner(input).into()
+}
 
-    let mut iter = args.iter();
+fn option_dotenv_inner(input: TokenStream2) -> TokenStream2 {
+    let args = match syn::parse2::<DotenvInput>(input) {
+        Ok(a) => a,
+        Err(e) => return e.into_compile_error(),
+    };
 
-    let var_name = iter
-        .next()
-        .ok_or_else(|| syn::Error::new(args.span(), "dotenv! takes 1 or 2 arguments"))?
-        .value();
-    let err_msg = iter.next();
+    let sequence = if args.override_ {
+        EnvSequence::EnvThenInput
+    } else {
+        EnvSequence::InputThenEnv
+    };
 
-    if iter.next().is_some() {
-        return Err(syn::Error::new(
-            args.span(),
-            "dotenv! takes 1 or 2 arguments",
-        ));
-    }
+    let path = args.path.map_or_else(|| "./.env".to_owned(), |p| p.value());
 
-    match env::var(&var_name) {
-        Ok(val) => Ok(quote!(#val)),
-        Err(e) => Err(syn::Error::new(
-            var_name.span(),
-            err_msg.map_or_else(
-                || match e {
-                    VarError::NotPresent => {
-                        format!("environment variable `{var_name}` not defined")
-                    }
+    let loader = EnvLoader::new().path(&path).sequence(sequence).load();
 
-                    VarError::NotUnicode(s) => {
-                        format!("environment variable `{var_name}` was not valid Unicode: {s:?}",)
-                    }
-                },
-                LitStr::value,
-            ),
-        )),
+    match loader.as_ref().map(|l| l.get(&args.var_name.value())) {
+        Ok(Some(v)) => quote!(Some(#v)),
+        Ok(None) => quote!(None::<&str>),
+        Err(e) => {
+            if let dotenvy::Error::Io(ioe, _) = e {
+                if ioe.kind() == io::ErrorKind::NotFound {
+                    return quote!(None::<&str>);
+                }
+            }
+            let msg = e.to_string();
+            quote! {
+                compile_error!(#msg)
+            }
+        }
     }
 }
--- dotenv_codegen/tests/basic_dotenv_macro.rs
+++ dotenv_codegen/tests/basic_dotenv_macro.rs
@@ -1,16 +1,76 @@
 #[test]
 fn dotenv_works() {
+    // basic operation
     assert_eq!(dotenvy_macro::dotenv!("CODEGEN_TEST_VAR1"), "hello!");
+    assert_eq!(
+        dotenvy_macro::dotenv!("CODEGEN_TEST_VAR2"),
+        "'quotes within quotes'"
+    );
+
+    // basic operation specifying var attribute
+    assert_eq!(dotenvy_macro::dotenv!(var = "CODEGEN_TEST_VAR1"), "hello!");
+
+    // overriding works
+    assert_eq!(
+        dotenvy_macro::dotenv!("CODEGEN_TEST_VAR1", override_ = true),
+        "hello!"
+    );
+
+    // not overriding also works, requires env vars to be set upon running test
+    assert_eq!(
+        dotenvy_macro::dotenv!(var = "CODEGEN_TEST_VAR1", override_ = false),
+        "goodbye!",
+        "in order for dotenvy_macro tests to pass, the variable `CODEGEN_TEST_VAR1` must be set to `goodbye!`"
+    );
+
+    // custom .env path works
+    assert_eq!(
+        dotenvy_macro::dotenv!("CODEGEN_TEST_VAR1", path = ".env.alternative"),
+        "bye!"
+    );
+
+    // custom .env path works while not overriding
+    assert_eq!(
+        dotenvy_macro::dotenv!("CODEGEN_TEST_VAR1", path = ".env.alternative", override_ = false),
+        "goodbye!",
+        "in order for dotenvy_macro tests to pass, the variable `CODEGEN_TEST_VAR1` must be set to `goodbye!`"
+    );
 }
 
 #[test]
-fn two_argument_form_works() {
+fn dotenv_option_works() {
+    // basic operation
     assert_eq!(
-        dotenvy_macro::dotenv!(
-            "CODEGEN_TEST_VAR2",
-            "err, you should be running this in the 'dotenv_codegen' \
-             directory to pick up the right .env file."
-        ),
-        "'quotes within quotes'"
+        dotenvy_macro::option_dotenv!("CODEGEN_TEST_VAR1"),
+        Some("hello!")
+    );
+    assert_eq!(
+        dotenvy_macro::option_dotenv!("CODEGEN_TEST_VAR_UNSET"),
+        None
+    );
+
+    // overriding works
+    assert_eq!(
+        dotenvy_macro::option_dotenv!("CODEGEN_TEST_VAR1", override_ = true),
+        Some("hello!")
+    );
+
+    // not overriding also works, requires env vars to be set upon running test
+    assert_eq!(
+        dotenvy_macro::option_dotenv!("CODEGEN_TEST_VAR1", override_ = false),
+        Some("goodbye!"),
+        "in order for dotenvy_macro tests to pass, the variable `CODEGEN_TEST_VAR1` must be set to `goodbye!`"
+    );
+
+    // custom .env path works
+    assert_eq!(
+        dotenvy_macro::option_dotenv!("CODEGEN_TEST_VAR1", path = "./.env.alternative"),
+        Some("bye!")
+    );
+
+    // missing custom .env path returns None
+    assert_eq!(
+        dotenvy_macro::option_dotenv!("CODEGEN_TEST_VAR1", path = "./.doesnt_exist"),
+        None
     );
 }
